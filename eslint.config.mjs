a4a5b30870e7d5528f375d6e4873978f31305b79
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-08-14 17:12:37 -0400

    config eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,60 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }, 
+  ignorePatterns: ['eslint.config.mjs'],
+  rules: {
+    '@typescript-eslint/no-unnecessary-condition': [
+      'error',
+      { allowRuleToRunWithoutStrictNullChecksIKnowWhatIAmDoing: true },
+    ],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    'import/no-unresolved': 'error',
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true, // don"t want to sort import lines, use eslint-plugin-import instead
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: true,
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+          'unknown',
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
+  },
+},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
