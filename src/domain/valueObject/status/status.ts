1cef5d50fa572275e82141db42314960931f0385
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-08-14 18:33:35 -0400

    feat: add status value object & test

---
+++ src/domain/valueObject/status/status.ts
@@ -0,0 +1,27 @@
+/**
+ * Status:
+ * todo, in-progress, done
+ */
+
+import { TaskStatus } from "../../interfaces";
+
+export class Status {
+    public status: TaskStatus
+
+    constructor(status: TaskStatus) {
+        this.status = status
+        this.validate()
+    }
+
+    isValidStatus(): boolean {
+        return Object.values(TaskStatus).includes(this.status)
+    }
+
+    validate() {
+        if (!this.isValidStatus()) {
+            throw new Error('Invalid status')
+        }
+
+        return true
+    }
+}
