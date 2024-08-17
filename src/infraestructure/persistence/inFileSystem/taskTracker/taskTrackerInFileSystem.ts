faa75066f8a0b91cce54026b8eb72101d53a5a50
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-08-17 12:02:27 -0400

    feat: add write file

--- src/infraestructure/persistence/inFileSystem/taskTracker/taskTrackerInFileSystem.ts
+++ src/infraestructure/persistence/inFileSystem/taskTracker/taskTrackerInFileSystem.ts
@@ -1,10 +1,9 @@
 import fs from 'node:fs/promises';
 import {
-  ICreateTask,
   IPersistenceInFileSystemAsync,
   ITask,
-  TaskStatus,
 } from '../../../../domain/interfaces';
+import { ConflitError } from '../../../../domain/exceptions';
 
 export class TaskTrackerInFileSystem
   implements IPersistenceInFileSystemAsync<ITask>
@@ -21,12 +20,20 @@ export class TaskTrackerInFileSystem
   }
 
   async read(): Promise<ITask[]> {
-    const data = await fs.readFile(this.filePath, 'utf8');
-    return JSON.parse(data);
+    try {
+      const data = await fs.readFile(this.filePath, 'utf8');
+      return JSON.parse(data);
+    } catch (error) {
+      throw new ConflitError('Error read file');
+    }
   }
 
   async write(data: ITask[]): Promise<void> {
-    const taskObjectToString = JSON.stringify(data);
-    await fs.writeFile(this.filePath, taskObjectToString, 'utf-8');
+    try {
+      const taskObjectToString = JSON.stringify(data);
+      await fs.writeFile(this.filePath, taskObjectToString, 'utf-8');
+    } catch (error) {
+      throw new ConflitError('Error write file');
+    }
   }
 }
